16b7c9888889e1c1e40caf1fdecf68a72815cac4
Michael Craig <mikethomascraig@gmail.com>
2025-07-24 12:06:24 +0100

    Completed up to the end of S24 - Express.js

--- res/24/3.4 Middleware/index4.js
+++ res/24/3.4 Middleware/index4.js
@@ -1,7 +1,30 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var genBandName = '';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function generateBandName (req, res, next) {
+  genBandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(generateBandName)
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is:</h1><h2>${genBandName}!</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- res/24/3.5 Secrets Project/index.js
+++ res/24/3.5 Secrets Project/index.js
@@ -1,3 +1,44 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+// body-parser is apparently now included in express so can
+// actually get away with removing that import and swapping
+// bodyParser with express throughout
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var pwdValid = false;
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+function pwdCheck (req, res, next) {
+    pwdValid = (req.body["password"] === "ILoveProgramming");
+    next();
+}
+
+app.use(pwdCheck);
+
+app.post('/check', (req, res) => {
+    if (pwdValid === true) {
+        res.sendFile(__dirname + '/public/secret.html');
+    } else {
+        res.sendFile(__dirname + '/public/index.html');
+    }
+})
+
+app.listen(port ,() => {
+    console.log(`Listening on port ${port}`)
+})
