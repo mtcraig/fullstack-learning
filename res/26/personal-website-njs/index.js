d9726bdbaa02fe0d41b8abf4972b49e423c697fa
Michael Craig <mikethomascraig@gmail.com>
2025-07-29 11:31:26 +0100

    Work in progress continuing updates the website ejs with the nav bar mostly sorted. Issues carrying things into a folder structure calling resources in other places.

--- res/26/personal-website-njs/index.js
+++ res/26/personal-website-njs/index.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import fs from 'fs';
+import path from 'path';
 import ejs from 'ejs';
 
 const app = express();
 const port = 3000;
+const viewsDir = path.resolve() + '/views/';
+const publicDir = path.resolve() + '/public/';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 function readJSON(target) {
-    return JSON.parse(fs.readFileSync(`public/content/resume/${target}.json`
+    return JSON.parse(fs.readFileSync(`public/content/${target}.json`
                 ,'utf-8'
                 ,(err,data) => {
                     if (err) {
@@ -26,25 +29,45 @@ app.use(express.static('public'));
 // app.use(readJSON);
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    var dataNav = readJSON('nav/navigation');
+    var activeNav = '';
+    res.render('index.ejs', {
+        viewsDir,
+        publicDir,
+        dataNav,
+        activeNav
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.render('about.ejs');
+    var dataNav = readJSON('nav/navigation');
+    var activeNav = 'about';
+    res.render('about.ejs', {
+        viewsDir,
+        publicDir,
+        dataNav,
+        activeNav
+    });
 })
 
 app.get('/resume', (req, res) => {
-    var dataSumm = readJSON('summary');
-    var dataHist = readJSON('history');
-    var dataQual = readJSON('qualifications')['qualifications'];
-    var dataTech = readJSON('tech-skills');
-    var dataDomn = readJSON('domain-skills')['Domain Skills'];
-    var dataAssn = readJSON('assignments')['Assignments'];
+    var dataNav = readJSON('nav/navigation');
+    var activeNav = 'resume';
+    var dataSumm = readJSON('resume/summary');
+    var dataHist = readJSON('resume/history');
+    var dataQual = readJSON('resume/qualifications')['qualifications'];
+    var dataTech = readJSON('resume/tech-skills');
+    var dataDomn = readJSON('resume/domain-skills')['Domain Skills'];
+    var dataAssn = readJSON('resume/assignments')['Assignments'];
     // dataTech['SAS Specialist'].forEach((item) => {
     //     console.log(item);
     // })
     // console.log(dataTech[0]);
     res.render('resume.ejs', {
+        viewsDir,
+        publicDir,
+        dataNav,
+        activeNav,
         dataSumm,
         dataHist,
         dataQual,
@@ -55,11 +78,36 @@ app.get('/resume', (req, res) => {
 })
 
 app.get('/portfolio', (req, res) => {
-    res.render('portfolio.ejs');
+    var dataNav = readJSON('nav/navigation');
+    var activeNav = 'portfolio';
+    res.render('portfolio.ejs', {
+        viewsDir,
+        publicDir,
+        dataNav,
+        activeNav
+    });
+})
+
+app.get('/portfolio/ppp-sweepstakes', (req, res) => {
+    var dataNav = readJSON('nav/navigation');
+    var activeNav = 'portfolio/ppp-sweepstakes';
+    res.render('portfolio/ppp-sweepstakes.ejs', {
+        viewsDir,
+        publicDir,
+        dataNav,
+        activeNav
+    });
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact.ejs');
+    var dataNav = readJSON('nav/navigation');
+    var activeNav = 'contact';
+    res.render('contact.ejs', {
+        viewsDir,
+        publicDir,
+        dataNav,
+        activeNav
+    });
 })
 
 app.listen(port, () => {
