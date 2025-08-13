c43c5506c08379a245915a5550a8d0f40eca4e76
Michael Craig <mikethomascraig@gmail.com>
2025-08-13 16:26:36 +0100

    Remove 9.2, not required. Implement hasing in 9.1.

--- res/35/9.1 Authentication Lv.1/index.js
+++ res/35/9.1 Authentication Lv.1/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 dotenv.config({ path: './config/.env' });
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
   user: String(process.env.user),
@@ -34,24 +36,29 @@ app.get("/register", (req, res) => {
 
 app.post("/register", async (req, res) => {
   const email = req.body.username
-  const password = req.body.password;
+  const password = req.body.password
   try {
     const check = await db.query("SELECT user_email FROM users WHERE user_email = $1", [email]);
     if (check.rows.length > 0) {
       return res.status(400).send("User already exists");
     } else {
-      console.log("User does not exist, proceeding with registration.");
+      try {
+        const hash = await bcrypt.hash(password, saltRounds);
+        try {
+          await db.query("INSERT INTO users (user_email, user_password) VALUES ($1, $2)", [email, hash]);
+          res.render("secrets.ejs");
+        } catch (err) {
+          console.error("Error inserting user:", err);
+          return res.status(500).send("Error registering user");
+        }
+      } catch (err) {
+        console.error("Error hashing password:", err);
+        return res.status(500).send("Error hashing password");
+      }
     }
   } catch (err) {
     console.error("Error checking user existence:", err);
     return res.status(500).send("Error checking user existence");
-  }
-  try {
-    await db.query("INSERT INTO users (user_email, user_password) VALUES ($1, $2)", [email, password]);
-    res.render("secrets.ejs");
-  } catch (err) {
-    console.error("Error inserting user:", err);
-    return res.status(500).send("Error registering user");
   }
 });
 
@@ -59,18 +66,26 @@ app.post("/login", async (req, res) => {
   const email = req.body.username
   const password = req.body.password;
   try {
-    const check = await db.query("select user_email from users where user_email = $1", [email]);
+    const check = await db.query("select user_email, user_password from users where user_email = $1", [email]);
     if (check.rows.length > 0) {
       const user = check.rows[0];
       console.log("User found:", user);
-      if (user.user_password !== password) {
-        return res.status(401).send("Invalid password");
-      } else {
-        console.log("Password is valid, logging in user.");
-        res.render("secrets.ejs");
-      }
+      bcrypt.compare(password, user.user_password, (err, result) => {
+        if (err) {
+          console.error("Error comparing passwords:", err);
+          return res.status(500).send("Error logging in user");
+        } else {
+          if (result) {
+            console.log("Password is valid, logging in user.");
+            res.render("secrets.ejs");
+          } else {
+            return res.status(401).send("Invalid password");
+          }
+        }
+      })
     } else {
-      return res.status(401).send("Invalid username");
+      console.log("User not found, redirecting to register.");
+      return res.status(404).send("User not found, please register");
     }
   } catch (err) {
     console.error("Error logging in user:", err);
